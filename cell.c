ac61a69cd3c5f0bcbeb6ddd9262d9937f3315a7c
Copni <nicolas.papleux@efrei.net>
2023-11-16 10:26:03 +0100

    Question 1 partie 1

--- cell.c
+++ cell.c
@@ -5,3 +5,9 @@
 #include <stdlib.h>
 #include "cell.h"
 
+t_d_cell * create_cell(int value, int lvl){
+    t_d_cell cell;
+    cell.level = (t_d_cell *)malloc(lvl * sizeof(t_d_cell)); //creation of a dynamic array of lvl levels
+    cell.value = value;
+    return &cell;
+}
